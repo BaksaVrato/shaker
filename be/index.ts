564ad3bd5c02fcbcec06be22b22a4cfd455706e0
BaksaVrato <vratusbaksa@gmail.com>
2024-07-27 17:56:22 +0200

    rewind and install dotenv

--- be/index.ts
+++ be/index.ts
@@ -1,2 +1,25 @@
-const greeting: string = "Hello, TypeScript!";
-console.log(greeting);
+console.log('Running...');
+
+const express = require('express');
+const mongoose = require('mongoose');
+
+
+const connectDB = require('./db/db');
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+  //res.send("Express + TypeScript Server");
+});
+
+
+
+connectDB();
+
+mongoose.connection.once('open', () => {
+  app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
+  });
+});
