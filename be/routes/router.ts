fa06526a9bf6dc13a6a935be8559897b101ef4d1
BaksaVrato <vratusbaksa@gmail.com>
2024-08-01 18:44:35 +0200

    BE deleted - app will be fully FE

--- be/routes/router.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-
-const controller = require("../controller/controller");
-const router = express.Router();
-
-router.post('/searchCocktailIdea', controller.searchCocktailIdea);
-
-module.exports = router;
